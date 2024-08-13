5d3e68edc431d8301be7c6ebc48c26bd055ab9d7
CrazyEnvelope <stanciustefan500@gmail.com>
2024-08-13 05:29:49 -0700

    Add scrollbars.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 from tkinter import filedialog as fd
 from PIL import Image, ImageTk
 
@@ -49,17 +48,40 @@ uploadWatermark.grid(row = 2, column = 1)
 watermarkPath = tk.Label(text = "Please, upload a watermark image!")
 watermarkPath.grid(row = 3,column = 1)
 
-canvasPicture = tk.Canvas()
-canvasPicture.config(width = 400, height = 400, background = "white")
+
+#################### CANVASFORTHEPICTURE ####################
+canvas_framePicture = tk.Frame(window)
+canvas_framePicture.grid(row=4, column=0)
+
+canvasPicture = tk.Canvas(canvas_framePicture, width = 400, height = 400, background = "white", scrollregion = (0,0,2000,2000))
 pictureText = canvasPicture.create_text(200,200, text = "Uploaded picture will\nbe shown here!", font = ("Arial", 20, "bold"))
-canvasPicture.grid(row = 4, column = 0)
+canvasPicture.grid(row = 0, column = 0)
 
-canvasWatermark = tk.Canvas()
-canvasWatermark.config(width = 400, height = 400, background = "white")
+xscrolPicture = tk.Scrollbar(canvas_framePicture, orient = "horizontal", command=canvasPicture.xview)
+xscrolPicture.grid(row=1,column = 0, sticky ='ew')
+canvasPicture.config(xscrollcommand=xscrolPicture.set)
+
+yscrollPicture = tk.Scrollbar(canvas_framePicture, orient="vertical", command=canvasPicture.yview)
+yscrollPicture.grid(row=0, column=1, sticky='ns')
+canvasPicture.config(yscrollcommand=yscrollPicture.set)
+
+#################### CANVASFORTHEWATERMARK ####################
+canvas_frameWatermark = tk.Frame(window)
+canvas_frameWatermark.grid(row=4, column=2)
+
+canvasWatermark = tk.Canvas(canvas_frameWatermark, width = 400, height = 400, background = "white", scrollregion = (0,0,2000,2000))
 watermarkText = canvasWatermark.create_text(200,200, text = "Uploaded watermark will\nbe shown here!", font = ("Arial", 20, "bold"))
-canvasWatermark.grid(row = 4, column = 2)
+canvasWatermark.grid(row = 0, column = 0)
 
-placeWatermark = tk.Button(text = "Place watermark!")
-placeWatermark.grid(row = 5, column = 1)
+xscrolWatermark = tk.Scrollbar(canvas_frameWatermark, orient = "horizontal", command=canvasWatermark.xview)
+xscrolWatermark.grid(row=1,column = 0, sticky ='ew')
+canvasWatermark.config(xscrollcommand=xscrolWatermark.set)
+
+yscrollWatermark = tk.Scrollbar(canvas_frameWatermark, orient="vertical", command=canvasWatermark.yview)
+yscrollWatermark.grid(row=0, column=1, sticky='ns')
+canvasWatermark.config(yscrollcommand=yscrollWatermark.set)
+
+# placeWatermark = tk.Button(text = "Place watermark!")
+# placeWatermark.grid(row = 6, column = 1)
 
 window.mainloop()
